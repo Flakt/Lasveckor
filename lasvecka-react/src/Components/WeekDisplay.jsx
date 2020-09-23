edda2dc2b17547d8c2a8e342f3a3ce016adb9321
Stefan Chan <stefan2846@hotmail.com>
2020-09-23 18:08:31 +0200

    Update WeekDisplay.jsx
    Api call over https instead of http

--- lasvecka-react/src/Components/WeekDisplay.jsx
+++ lasvecka-react/src/Components/WeekDisplay.jsx
@@ -28,7 +28,7 @@ class WeekDisplay extends Component {
   // Use localhost:5000/getData for dev
   componentDidMount() {
     axios
-      .get("http://api.lasvecka.nu/getData")
+      .get("https://api.lasvecka.nu/getData")
       .then(res => {
         this.setState({
           data: res.data
